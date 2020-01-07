5756c506196a098e441c2ca7cd1622c89588dc1f
Byungju Chae <cbyungjub@naver.com>
2020-01-08 01:04:48 +0900

    알고리즘 별 parameter 종류 및 개수를 처음에 return

--- CryptoView/BackEnd/main.py
+++ CryptoView/BackEnd/main.py
@@ -7,6 +7,28 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+# 암호화 과정에서 필요한 parameter의 종류와 개수를 return한다.
+@app.route('/enc/list', methods=['GET'])
+def returnEncParam():
+    response = {
+        "rsa": ['e', 'n'],
+        "DES": [],
+        "AES": [],
+        "ElGamal": []
+    }
+    return jsonify(response), 200
+
+# 복호화 과정에서 필요한 parameter의 종류와 개수를 return한다.
+@app.route('/dec/list', methods=['GET'])
+def returnDecParam():
+    response = {
+        "rsa": ['d', 'n'],
+        "DES": [],
+        "AES": [],
+        "ElGamal": []
+    }
+    return jsonify(response), 200
+
 @app.route('/test', methods=['GET'])
 def test():
     response = {
@@ -17,16 +39,16 @@ def test():
 @app.route('/dec/rsa', methods=['POST', 'GET'])
 def post():
     result = request.get_json(silent=True)
-    print(result)
-    param = [
-        int(result['d']),
-        int(result['n']),
-        int(result['ciphertext'])
-    ]
+    param = {
+        'd': int(result['d']),
+        'n': int(result['n']),
+        'ciphertext': int(result['ciphertext'])
+    }
+    
     response = {
         "plaintext": str(RSA.decrypt(param))
     }
-    return jsonify(response)
+    return jsonify(response), 200
 
 
 # Running the App
