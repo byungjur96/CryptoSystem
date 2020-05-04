216408211b07eae3ad4eb91798db2a9dce302f32
Byungju Chae <cbyungjub@naver.com>
2020-05-04 17:05:34 +0900

    Key Generator Visualize

--- CryptoView/FrontEnd/src/components/keygen.vue
+++ CryptoView/FrontEnd/src/components/keygen.vue
@@ -3,7 +3,14 @@
     <div class="layout" @click="closeModal()"></div>
     <div class="pop-up">
       <h3>Key Generator</h3>
-      <div id="key" v-if="keyExist()">{{ key }}</div>
+      <div id="key-result" v-if="keyExist()">
+        <div v-for="(_, name) in key" v-bind:key="name">
+          <div class="key-title">{{ name }}</div>
+          <div class="key-value" v-for="value in key[name]" v-bind:key="value">
+          {{ value }}
+          </div>
+        </div>
+      </div>
       <button @click="generateKey()">Generate</button>
     </div>
   </div>
@@ -44,32 +51,53 @@ export default {
     width: 100%;
     height: 100%;
   }
-  .layout {
-    position: absolute;
-    width: 100vw;
-    height: 100vh;
-    background-color: black;
-    opacity: 0.5;
-  }
-  .pop-up {
-    position: absolute;
-    left: calc(50vw - 100px);
-    top: calc(50vh - 150px);
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    align-items: center;
-    border: 1px solid black;
-    width: 200px;
-    height: 300px;
-    background-color: white;
-  }
 
-  .pop-up > h3 {
-    margin-top: 20px;
-  }
+#key-result {
+  width: calc(100% - 40px);
+  padding: 20px;
+  padding-top: 0;
+  margin-bottom: 20px;
+  margin-left: 0;
+  margin-right: 0;;
+}
 
-  .pop-up > button {
-    margin-bottom: 30px;
-  }
+.key-title {
+  font-weight: bold;
+  font-size: 16px;
+  margin-bottom: 10px;
+}
+
+.key-value {
+  margin-bottom: 10px;
+  font-size: 14px;
+}
+
+.layout {
+  position: absolute;
+  width: 100vw;
+  height: 100vh;
+  background-color: black;
+  opacity: 0.5;
+}
+.pop-up {
+  position: absolute;
+  left: calc(50vw - 100px);
+  top: calc(50vh - 150px);
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  border: 1px solid black;
+  width: 200px;
+  height: 300px;
+  background-color: white;
+}
+
+.pop-up > h3 {
+  margin-top: 20px;
+}
+
+.pop-up > button {
+  margin-bottom: 30px;
+}
 </style>
